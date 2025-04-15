3f8008b16c60de8bbb9f106f61c276e815ae40a0
naveedrahid <naveedrashid685@gmail.com>
2025-04-15 20:07:37 +0500

    add new migrations

--- database/migrations/2025_04_15_150551_create_expenses_table.php
+++ database/migrations/2025_04_15_150551_create_expenses_table.php
@@ -13,12 +13,6 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('employee_id')->constrained()->onDelete('cascade');
-            $table->decimal('amount', 10, 2);
-            $table->string('expense_type');
-            $table->text('description')->nullable();
-            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
